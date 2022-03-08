006285850a7d48a79f8f3da848335944f8e8c614
Bill Han <billbrownbear23@gmail.com>
2022-03-08 23:51:35 +0900

    feat: created module that contains Notion autherization token and TDL DB id constants

---
+++ notion/constants.py
@@ -0,0 +1,3 @@
+autherization = "secret_RySJ7IpnyDR3wbCk6rLG5MFxr263gxEPz1LbtQxhaLw"
+# database_id = "48e88dbf8cef4d01b7a47474c35e0f07" #sample
+database_id = "9f21bb21b21b44c48d81d082ec605f37" #real
