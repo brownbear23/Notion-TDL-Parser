d33459c248d910209e86d772cc09f813a1e78835
Bill Han <billbrownbear23@gmail.com>
2022-05-05 12:17:07 +0900

    feat: adds entrypoint

---
+++ notion_tdl_parser.py
@@ -0,0 +1,16 @@
+from notion.parser import board_parser
+from notion.ms_word_writer import writer
+from datetime import datetime
+import sys
+
+tdl_template_doc_dir = "/Users/billhan/Desktop/Dev/Notion-TDL-Parser/notion/ms_word_writer/tdl_template.docx"
+doc_name = datetime.now().strftime("TDL_%y%m%d_%H%M")
+print("Parsing " + doc_name + ".docx ...")
+
+board_list = board_parser.parse_board()
+
+write_dir = sys.argv[1]
+writer.tdl_writer(tdl_template_doc_dir, write_dir+"/"+doc_name, board_list)
+
+
+
