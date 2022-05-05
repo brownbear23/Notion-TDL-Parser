99afc2bb90f4d1a39eb458c7791f73e94445ad5e
Bill Han <billbrownbear23@gmail.com>
2022-05-05 12:27:51 +0900

    refactor: updates internal integration token

--- notion/constants.py
+++ notion/constants.py
@@ -3,8 +3,7 @@ from docx.shared import Pt
 
 ### Notion related constants
 class NotionConstants:
-    authorization = "secret_RySJ7IpnyDR3wbCk6rLG5MFxr263gxEPz1LbtQxhaLw"
-    # database_id = "48e88dbf8cef4d01b7a47474c35e0f07" #sample
+    authorization = "your authorization token"
     database_id = "9f21bb21b21b44c48d81d082ec605f37"  # real
 
 
